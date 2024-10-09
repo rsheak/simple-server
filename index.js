31dd5a226b662a7cb5d51c0fa193b7418e6c5019
rsheak <rshea001@campus.goldsmiths.ac.uk>
2024-10-09 11:40:46 +0100

    new code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = 8000;
 
 http.createServer(function(req, res) { 
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World!");
+    res.end("Hello Worldddddd!");
     }).listen(port, function() { 
         console.log(`Node server is running on port ${port}...`); 
 }); 
